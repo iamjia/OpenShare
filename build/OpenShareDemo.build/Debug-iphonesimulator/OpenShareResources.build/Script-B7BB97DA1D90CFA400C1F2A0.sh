c06f9cf5be92034a167d974333cceff5cb2cffb4
dake <chen423@yeah.net>
2017-04-20 10:40:12 +0800

    rm build

--- build/OpenShareDemo.build/Debug-iphonesimulator/OpenShareResources.build/Script-B7BB97DA1D90CFA400C1F2A0.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-BUILD_BUNDLE_DIR=${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.bundle
-TARGET_DIR=${SRCROOT}/build/${PRODUCT_NAME}.bundle
-
-rm -rf "$TARGET_DIR"
-cp -rf "$BUILD_BUNDLE_DIR" "$TARGET_DIR"
